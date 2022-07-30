174ddec9c709767288280c3aafe81ccd75689ede
Устюгов <alexust11@yandex.ru>
2022-07-30 16:13:11 +0700

    Исправил текст при выводе на экран

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Выполнение Домашнего задания");
+        System.out.println("Выполнение Домашнего задания 1");
     }
 }
